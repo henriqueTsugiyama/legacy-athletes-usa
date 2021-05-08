ce3378e2c4e970da077ceb8dd4c699c3188fe2b9
henriqueTsugiyama <henrique-tsu@hotmail.com>
2021-05-08 17:56:05 -0300

    added new pictures, responsiveness, localstorage, and togglebutton

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,17 +6,17 @@ export const Header = (props) => {
           <div className='container'>
             <div className='row'>
               <div className=' col-xs-12 col-md-8 intro-text'>
-                {/* <h1>
+                <h1>
                   {props.data ? props.data.title : 'Loading'}
                   <span></span>
-                </h1> */}
-                {/* <p>{props.data ? props.data.paragraph : 'Loading'}</p>
+                </h1>
+                <p>{props.data ? props.data.paragraph : 'Loading'}</p>
                 <a
                   href='#features'
                   className='btn btn-custom btn-lg page-scroll'
                 >
                   Learn More 
-                </a>{' '}*/}
+                </a>{' '}
               </div>
             </div>
           </div>
